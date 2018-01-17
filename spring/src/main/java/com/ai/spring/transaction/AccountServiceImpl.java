b3856387c501e37a67acbcaec26c3d6ca4929200
MicroKibaco <569015640@qq.com>
2018-01-17 19:09:54 +0800

    Spring BeanCreationException

--- spring/src/main/java/com/ai/spring/transaction/AccountServiceImpl.java
+++ spring/src/main/java/com/ai/spring/transaction/AccountServiceImpl.java
@@ -1,37 +1,51 @@
 package com.ai.spring.transaction;
 
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
 /**
  * 转账业务层的实现类
  */
 public class AccountServiceImpl implements AccountService {
 
     // 注入我们转账的DAO类
+    private AccountDao mAccountDao;
 
-    public AccountDao getAccountDao() {
-        return mAccountDao;
-    }
+    // 注入事务管理的模板
+    private TransactionTemplate mTransactionTemplate;
+
 
     public void setAccountDao(AccountDao accountDao) {
         mAccountDao = accountDao;
     }
 
-    private AccountDao mAccountDao;
+    public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+        this.mTransactionTemplate = transactionTemplate;
+    }
 
     public AccountServiceImpl(AccountDao accountDao) {
-        mAccountDao = accountDao;
+        this.mAccountDao = accountDao;
     }
 
     public AccountServiceImpl() {
     }
-
 
     /**
      * @param out   :转出的账号
      * @param in    :转入的账号
      * @param money :转账金额
      */
-    public void transfer(String out, String in, Double money) {
-        mAccountDao.outMoney(out, money);
-        mAccountDao.inMoney(in, money);
+    public void transfer(final String out, final String in, final Double money) {
+
+        mTransactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                mAccountDao.outMoney(out, money);
+            //    int i = 1 / 0;
+                mAccountDao.inMoney(in, money);
+            }
+        });
     }
+
+
 }
