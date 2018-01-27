048e8a704c25a1b4420cffcec5b284ae3867abe1
MicroKibaco <569015640@qq.com>
2018-01-27 10:40:50 +0800

    自动查询与回复

--- mybatis/src/main/java/com/ai/servlet/AutoReplyServlet.java
+++ mybatis/src/main/java/com/ai/servlet/AutoReplyServlet.java
@@ -1,24 +1,30 @@
 package com.ai.servlet;
 
+import com.ai.service.QueryService;
+
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
- * 对话页的初始化控制
+ * 自动回复功能控制层
  */
 @SuppressWarnings("serial")
-public class InitTalkServlet extends HttpServlet {
+public class AutoReplyServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		// 设置编码
-		req.setCharacterEncoding("UTF-8");
-		// 向页面跳转
-		req.getRequestDispatcher("/WEB-INF/jsp/front/talk.jsp").forward(req, resp);
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		QueryService queryService = new QueryService();
+		out.write(queryService.queryByCommand(req.getParameter("content")));
+		out.flush();
+		out.close();
 	}
 	
 	@Override
